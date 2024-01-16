4c849fe6c5d99fb367e7a0aca1302715a2b185b6
JiHyeok Heo <vndj1212@gmail.com>
2024-01-16 23:59:26 +0900

    Ui Event Handler , UI_Base, UI_Button(Instance)
    이벤트 핸들러를 활용해 Drag, Click 이벤트 활성화
    람다 + Action을 활용한 옵저버 패턴

--- U1_Project/Assets/Scripts/UI/UI_Button.cs
+++ U1_Project/Assets/Scripts/UI/UI_Button.cs
@@ -1,19 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
-    [SerializeField]
-    TextMeshProUGUI _text;
+    enum Buttons
+    {
+        PointButton,
+    }
+
+    enum Texts
+    {
+        PointText,
+        ScoreText,
+    }
+
+    enum GameObjects
+    {
+        TestObject,
+    }
+
+    enum Images
+    {
+        ItemIcon,
+    }
+
+    private void Start()
+    {
+        // 리플렉션
+        Bind<Button>(typeof(Buttons));
+        Bind<TextMeshProUGUI>(typeof(Texts));
+        Bind<GameObject>(typeof(GameObjects));
+        Bind<Image>(typeof(Images));
+
+        //Get<TextMeshProUGUI>((int)Texts.ScoreText).text = "Bind Test";
+        GetText((int)Texts.ScoreText).text = "Bind Test";
+
+        GameObject obj = GetImage((int)Images.ItemIcon).gameObject;
+        UI_EventHandler evt = obj.GetComponent<UI_EventHandler>();
+        evt.OnDragHandler += ((PointerEventData data) => { evt.gameObject.transform.position = data.position; });
+    }
+
+   
     int _score = 0;
 
     public void OnButtonClicked()
     {
-        Debug.Log("Button Clicked@");
-
         _score++;
-        _text.text = $"Score : {_score}";
     }
 }
