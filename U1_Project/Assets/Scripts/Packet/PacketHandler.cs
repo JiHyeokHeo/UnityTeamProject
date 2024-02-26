620e94e4e6c064e776f6c98899c6f3197525fb3c
JiHyeok Heo <vndj1212@gmail.com>
2024-02-26 18:50:44 +0900

    이동동기화 완료

--- U1_Project/Assets/Scripts/Packet/PacketHandler.cs
+++ U1_Project/Assets/Scripts/Packet/PacketHandler.cs
@@ -9,22 +9,35 @@ using UnityEngine;
 
 class PacketHandler
 {
-    public static void S_ChatHandler(PacketSession session, IPacket packet)
+    public static void S_BroadcastEnterGameHandler(PacketSession session, IPacket packet)
     {
-        S_Chat chatPacket = packet as S_Chat;
+        S_BroadcastEnterGame pkt = packet as S_BroadcastEnterGame;
         ServerSession serverSession = session as ServerSession;
 
-        //if (chatPacket.playerId == 1)
-        {
-            Debug.Log(chatPacket.chat);
+        TestPlayerManager.Instance.EnterGame(pkt);
+    }
 
-            GameObject obj = GameObject.Find("Player");
-            if (obj == null)
-                Debug.Log("Player not found");
-            else
-                Debug.Log("Player found");
-        }
+    public static void S_BroadcastLeaveGameHandler(PacketSession session, IPacket packet)
+    {
+        S_BroadcastLeaveGame pkt = packet as S_BroadcastLeaveGame;
+        ServerSession serverSession = session as ServerSession;
 
-            //Console.WriteLine(chatPacket.chat);
+        TestPlayerManager.Instance.LeaveGame(pkt);
+    }
+
+    public static void S_PlayerListHandler(PacketSession session, IPacket packet)
+    {
+        S_PlayerList pkt = packet as S_PlayerList;
+        ServerSession serverSession = session as ServerSession;
+
+        TestPlayerManager.Instance.Add(pkt);
+    }
+
+    public static void S_BroadcastMoveHandler(PacketSession session, IPacket packet)
+    {
+        S_BroadcastMove pkt = packet as S_BroadcastMove;
+        ServerSession serverSession = session as ServerSession;
+
+        TestPlayerManager.Instance.Move(pkt);
     }
 }
