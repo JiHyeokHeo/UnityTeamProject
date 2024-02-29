9c028d38ce49ca894dc1638fdccad461d153d5b8
TwoNight0 <wlsdn0658@gmail.com>
2024-02-29 22:48:45 +0900

    lol 모델이 glb로 추출되기때문에 스케일을 바꾸려면 blender 같은 외부툴을 사용해야함, 그리고 이 glb를 유니티에서 사용하려면 https://github.com/Siccity/GLTFUtility 이걸 이용하는 것이 일반적임

---
+++ U1_Project/Assets/Scripts/Unit/Unit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Unit : MonoBehaviour
+{
+    protected int numberCode;
+    protected float damage_physic;
+    protected float damage_magic;
+    protected float speed_move;
+    protected float defence_physic;
+
+}
